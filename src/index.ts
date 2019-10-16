2661657cef68ea55c4ff39d9fdf06096ceef1979
JiangWeixian <saber_wei@163.com>
2019-10-16 16:27:03 +0800

    feat: init use-async-cache

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,50 @@
-import ms from 'ms';
-import lunchtime from './lunchtime.js';
-import millisecondsUntil from './millisecondsUntil.js';
+import { useEffect, useState } from 'react'
 
-export default function howLongUntilLunch(hours: number = 12, minutes: number = 30): string {
-	const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
-	return ms(millisecondsUntilLunchTime, { long: true });
+const store: { [key: string]: unknown } = {}
+const queue: { [key: string]: ((value?: unknown) => void)[] } = {}
+const running: { [key: string]: boolean } = {}
+
+const fetch = async <T>(api: () => Promise<T>, id: string) => {
+  try {
+    const callbacks = queue[id]
+    queue[id] = []
+    running[id] = false
+    const res = store[id] ? store[id] : await api()
+    store[id] = res
+    callbacks.forEach(callback => {
+      callback(res)
+    })
+  } catch (e) {
+    console.error(e)
+    running[id] = false
+  } finally {
+    running[id] = false
+  }
+}
+
+export type UseAsyncCahceProps<T> = {
+  id: string
+  api(): Promise<T>
+}
+
+export const useAsyncCache = <T>(props: UseAsyncCahceProps<T>) => {
+  const [cached, setCached] = useState<T>()
+  useEffect(() => {
+    if (!props.id) {
+      return
+    }
+    new Promise<any>(resolve => {
+      if (!queue[props.id]) {
+        queue[props.id] = []
+      }
+      queue[props.id].push(resolve)
+      if (!running[props.id]) {
+        running[props.id] = true
+        setTimeout(() => fetch(props.api, props.id), 60)
+      }
+    }).then(setCached)
+  }, [props.id])
+  return {
+    cached,
+  }
 }
